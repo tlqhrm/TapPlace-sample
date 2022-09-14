12ce3b7e7dece0701f51277352386038b464a3d4
tlqhrm <85216782+tlqhrm@users.noreply.github.com>
2022-09-14 18:01:52 +0900

    store find

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -28,21 +28,18 @@ export class StoreController {
   async aroundStore(@Body() aroundStoreDto: AroundStoreDto) {
     return await this.storeService.aroundStore(aroundStoreDto);
   }
-
+  @Get(':store_id')
+  async fineOne(@Param('store_id') store_id: string) {
+    return await this.storeService.getStoreById(store_id);
+  }
+  @Get()
+  async findAll(): Promise<Store[]> {
+    return await this.storeService.findAll();
+  }
   //dev
   // @Post()
   // async create(@Body() createStoreDto: CreateStoreDto, @keyCheck(keyPipe) key) {
   //   return await this.storeService.create(createStoreDto);
-  // }
-
-  // @Get(':store_id')
-  // async fineOne(@Param('store_id') store_id: string) {
-  //   return await this.storeService.getStoreById(store_id);
-  // }
-
-  // @Get()
-  // async findAll(): Promise<Store[]> {
-  //   return await this.storeService.findAll();
   // }
 
   // @Delete(':id')
